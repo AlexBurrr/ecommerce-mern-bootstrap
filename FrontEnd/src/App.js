eec7965af36b5c3609d5e118849d50b774d874aa
Alex Berumen <alexandroberumen@gmail.com>
2021-04-16 19:59:49 -0700

    k

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main className='py-3' >
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+
+
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
